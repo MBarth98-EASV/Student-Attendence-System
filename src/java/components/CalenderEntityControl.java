7bdffcbc06a3d2c56ba29aef758d5148c4d1f4f1
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-02-08 08:53:23 +0100

    Still CSS errors

--- src/java/components/CalenderEntityControl.java
+++ src/java/components/CalenderEntityControl.java
@@ -18,16 +18,14 @@ public class CalenderEntityControl extends Pane
     {
         FXMLLoader fxmlLoader = new FXMLLoader(getResource("views\\CalenderEntity.fxml"));
 
-        //this.getStylesheets().add(this.getClass().getResource(".../styles/CourseEntity.css").toExternalForm());
+        //this.getStylesheets().add(this.getClass().getResource("styles/CourseEntity.css").toExternalForm());
         //this.getStylesheets().add(this.getClass().getResource("resources/styles/CourseEntity.css").toExternalForm());
 
         fxmlLoader.setControllerFactory(param -> controller = new CalenderEntityController());
 
-        try
-        {
+        try {
             view = fxmlLoader.load();
-        }
-        catch (IOException ignored) {}
+        } catch (IOException ignored) {}
 
         getChildren().add(view);
         controller.setLblStartTime(startTime);
