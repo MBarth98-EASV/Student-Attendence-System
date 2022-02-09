9eaebb5d18ed39ac305fdb6ff1af0535f5c34dfc
Mads Mandahl-Barth <mads-barth@live.dk>
2022-02-09 13:31:34 +0100

    animation

--- src/java/controllers/MainViewController.java
+++ src/java/controllers/MainViewController.java
@@ -1,22 +1,32 @@
 package controllers;
 
 import components.CalenderEntityControl;
+import javafx.animation.Interpolator;
+import javafx.animation.KeyFrame;
+import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Orientation;
 import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.layout.AnchorPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.TilePane;
+import javafx.scene.layout.*;
+import javafx.util.Duration;
 import util.Course;
 import util.EnumCourseStatus;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainViewController implements Initializable {
+
+    public Pane viewBodyContainer;
+    public GridPane mainViewBody;
 
     @FXML ScrollPane scrollPaneCourses;
     @FXML Label lblDay;
@@ -31,6 +41,23 @@ public class MainViewController implements Initializable {
     TilePane coursePane;
 
 
+    private void loadSecond(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/views/signInManuel.fxml"));
+        Scene scene = btnAttendLeave.getScene();
+        root.translateYProperty().set(scene.getHeight());
+
+        viewBodyContainer.getChildren().add(root);
+
+        Timeline timeline = new Timeline();
+        KeyValue kv = new KeyValue(root.translateYProperty(), 0, Interpolator.EASE_IN);
+        KeyFrame kf = new KeyFrame(Duration.seconds(10), kv);
+        timeline.getKeyFrames().add(kf);
+        timeline.setOnFinished(t -> {
+            viewBodyContainer.getChildren().remove(mainViewBody);
+        });
+        timeline.play();
+    }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
@@ -48,14 +75,19 @@ public class MainViewController implements Initializable {
         coursePane.getChildren().add(new CalenderEntityControl("0000", "0000", "COURSE 1", "Here", EnumCourseStatus.ATTENDED));
         coursePane.getChildren().add(new CalenderEntityControl("0000", "0000", "COURSE 1", "Here", EnumCourseStatus.ATTENDED));
         scrollPaneCourses.setContent(coursePane);
-
-
-
-
     }
 
 
-    public void onMainView(ActionEvent event) {
+    public void onMainView(ActionEvent event)
+    {
+        try
+        {
+            loadSecond(event);
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
     }
 
     public void onShowStats(ActionEvent event) {
