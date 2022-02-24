eeb09d3c358d5a7fea9b5825fb60e6e6c86000e8
sandbxk <90904706+sandbxk@users.noreply.github.com>
2022-02-24 19:16:21 +0100

    css rampage pt 1
    Since this is now meant to be a prototype, it's gonna be a damn pretty one

---
+++ src/java/controller/StatisticsTeacherP2Controller.java
@@ -0,0 +1,11 @@
+package controller;
+
+import javafx.scene.input.MouseEvent;
+
+public class StatisticsTeacherP2Controller {
+    public void switchPage1(MouseEvent mouseEvent) {
+    }
+
+    public void switchPage2(MouseEvent mouseEvent) {
+    }
+}
